c4cde108986ea4353b845e2ee2e955b4e836876c
tatia97 <Tsiskarauli84@gmail.com>
2022-01-20 23:49:22 +0400

    final project

---
+++ index.js
@@ -0,0 +1,34 @@
+let modal = document.getElementById("myModal");
+let btn = document.getElementById("myBtn");
+let span = document.getElementsByClassName("close")[0];
+
+
+
+
+
+function myFunction() {
+    
+}
+
+myFunction()
+
+
+btn.onclick = function() {
+  modal.style.display = "block";
+  let x = document.getElementById("email").value;
+ document.getElementById("demo").innerHTML = document.getElementById("demo").innerHTML + " " + x ;
+
+
+}
+
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
